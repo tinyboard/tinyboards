8e0171fc0c27ace66d80bb0c262cd88fbbcd25d4
kroner <kronusdev@protonmail.com>
2022-09-28 20:06:17 -0500

    added a UTC TimeStamp function, outputs current UTC time as a unix timestamp

--- crates/utils/src/time.rs
+++ crates/utils/src/time.rs
@@ -1,8 +1,17 @@
 use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::prelude::*;
 
 pub fn time() -> u64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_secs()
+}
+
+
+/**
+ *  Returns a UNIX Timestamp of the current time in the UTC TimeZone (i64)
+ */
+pub fn utc_timestamp() -> i64 {
+    Utc::now().timestamp()
 }
