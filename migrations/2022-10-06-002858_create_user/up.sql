4b85be3e0442352cfa8979811d95a21cdc7622fd
kroner <kronusdev@protonmail.com>
2022-10-05 19:33:09 -0500

    created user_ and user_ban table, seeded user_ table with dummy admin

---
+++ migrations/2022-10-06-002858_create_user/up.sql
@@ -0,0 +1,22 @@
+create table user_ (
+  id serial primary key,
+  name varchar(30) not null,
+  fedi_name varchar(40) not null,
+  preferred_name varchar(30),
+  passhash text not null,
+  email text unique,
+  admin boolean default false not null,
+  banned boolean default false not null,
+  published timestamp not null default now(),
+  updated timestamp,
+  unique(name, fedi_name)
+);
+
+create table user_ban (
+  id serial primary key,
+  user_id int references user_ on update cascade on delete cascade not null,
+  published timestamp not null default now(),
+  unique (user_id)
+);
+
+insert into user_ (name, fedi_name, passhash) values ('admin', 'porpl', 'porpl');
