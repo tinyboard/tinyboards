aed007fabaa98cdb4b8b8dcee226437c110896ce
kroner <kronusdev@protonmail.com>
2022-10-06 19:57:26 -0500

    fixed mod view migration

--- migrations/2022-10-07-001812_create_mod_views/down.sql
+++ migrations/2022-10-07-001812_create_mod_views/down.sql
@@ -1,1 +1,8 @@
--- This file should undo anything in `up.sql`
+drop view mod_remove_post_view;
+drop view mod_lock_post_view;
+drop view mod_remove_comment_view;
+drop view mod_remove_board_view;
+drop view mod_ban_from_board_view;
+drop view mod_ban_view;
+drop view mod_add_board_view;
+drop view mod_add_view;
