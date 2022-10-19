72de457c44ca957a12b2f76d22fe742ef0e1c1d0
kroner <kronusdev@protonmail.com>
2022-10-18 21:53:37 -0500

    init commit for comment functionality

--- crates/db/src/impls/mod.rs
+++ crates/db/src/impls/mod.rs
@@ -2,3 +2,4 @@ pub mod user;
 pub mod post;
 pub mod board;
 pub mod secret;
+pub mod comment;
--- crates/db/src/models/comment/comment.rs
+++ crates/db/src/models/comment/comment.rs
@@ -1,16 +1,31 @@
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
+use chrono::NaiveDateTime;
+use crate::schema::comment;
 
-#[derive(Queryable, Serialize, Deserialize, Debug, Clone)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[derive(Queryable, Identifiable)]
+#[diesel(table_name = comment)]
 pub struct Comment {
     pub id: i32,
     pub creator_id: i32,
     pub post_id: i32,
+    pub parent_id: Option<i32>,
     pub body: String,
     pub removed: bool,
-    pub published: chrono::NaiveDateTime,
-    pub updated: chrono::NaiveDateTime,
+    pub published: NaiveDateTime,
+    pub updated: Option<NaiveDateTime>,
     pub deleted: bool,
-    // #[serde(with = "LtreeDef")]
-    // pub path: Ltree,
+}
+
+#[derive(Clone, Default, Insertable, AsChangeset)]
+#[diesel(table_name = comment)]
+pub struct CommentForm {
+    pub creator_id: i32,
+    pub post_id: i32,
+    pub parent_id: Option<Option<i32>>,
+    pub body: Option<String>,
+    pub removed: Option<bool>,
+    pub updated: Option<NaiveDateTime>,
+    pub deleted: Option<bool>,
 }
