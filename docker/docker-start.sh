19f0ed630b39746c648246285c552b50c8413a7b
nunya <nunya@bidness.com>
2022-10-21 01:15:30 -0400

    docker n sheit

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -6,9 +6,11 @@
 set -e
 
 sudo docker-compose down
-sudo docker build ../ --file Dockerfile -t porpl-dev:latest
-sudo docker build ../../porpl-fe --file Dockerfile -t porpl-fe:latest
-#sudo docker build /home/bunchies/gits/porpl/porpl-fe --file Dockerfile -t porpl-fe:latest
-sudo docker-compose pull --ignore-pull-failures || true
-#sudo docker-compose up -d
+sudo docker-compose build --no-cache
 sudo docker-compose up
+
+#sudo docker build ../ --file Dockerfile -t porpl-dev:latest
+#sudo docker build ../../porpl-fe --file Dockerfile -t porpl-fe:latest
+#sudo docker build /home/bunchies/gits/porpl/porpl-fe --file Dockerfile -t porpl-fe:latest
+#sudo docker-compose pull --ignore-pull-failures || true
+#sudo docker-compose up -d
