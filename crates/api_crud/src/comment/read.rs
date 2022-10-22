1cdac33ac25bc7d283b19646dd8580be2b46fe85
Verstopfung <throwaway.546372819@protonmail.com>
2022-10-22 23:39:21 +0200

    stuff

--- crates/api_crud/src/comment/read.rs
+++ crates/api_crud/src/comment/read.rs
@@ -3,7 +3,7 @@ use actix_web::web;
 use porpl_api_common::{
     comment::{GetPostComments, GetPostCommentsRoute},
     data::PorplContext,
-    utils::blocking,
+    utils::{blocking, load_user_opt},
 };
 use porpl_db::models::post::post::Post;
 use porpl_db_views::{comment_view::CommentQuery, structs::CommentView};
@@ -18,8 +18,9 @@ impl<'des> PerformCrud<'des> for GetPostComments {
         self,
         context: &web::Data<PorplContext>,
         path: Self::Route,
-        _: Option<&str>,
+        auth: Option<&str>,
     ) -> Result<Self::Response, PorplError> {
+        let user = load_user_opt(context.pool(), context.master_key(), auth).await?;
         // check if post exists
         if blocking(context.pool(), move |conn| {
             Post::check_if_exists(conn, path.post_id)
@@ -36,6 +37,7 @@ impl<'des> PerformCrud<'des> for GetPostComments {
                 //.sort(None)
                 .post_id(Some(path.post_id))
                 .show_deleted_and_removed(Some(true))
+                .user(user.as_ref())
                 //.page(None)
                 //.limit(None)
                 .build()
