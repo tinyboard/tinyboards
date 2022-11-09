67fb78232665672f181e817c3010bd8e59a01003
kroner <kronusdev@protonmail.com>
2022-11-08 21:24:10 -0600

    logic updates for BoardBlock

--- crates/db/src/impls/board/board_block.rs
+++ crates/db/src/impls/board/board_block.rs
@@ -6,11 +6,30 @@ use crate::{
     traits::Blockable,  
 };
 
+
+impl BoardBlock {
+    pub fn read(
+        conn: &mut PgConnection,
+        for_user_id: i32,
+        for_board_id: i32,
+    ) -> Result<Self, TinyBoardsError> {
+        board_block
+            .filter(user_id.eq(for_user_id))
+            .filter(board_id.eq(for_board_id))
+            .first::<Self>(conn)
+            .map_err(|_e| TinyBoardsError::from_string("error reading board block", 500))
+    }
+}
+
+
 impl Blockable for BoardBlock {
     type Form = BoardBlockForm;
     fn block(conn: &mut PgConnection, form: &Self::Form) -> Result<Self, TinyBoardsError> {
         diesel::insert_into(board_block)
             .values(form)
+            .on_conflict((user_id, board_id))
+            .do_update()
+            .set(form)
             .get_result::<Self>(conn)
             .map_err(|_e| TinyBoardsError::from_string("could not block board", 500))
     }
