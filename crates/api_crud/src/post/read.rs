fab78cc738645efce2ca3b852d7a4acdedbe68b8
Verstopfung <throwaway.546372819@protonmail.com>
2022-11-10 11:45:55 +0100

    stuff

--- crates/api_crud/src/post/read.rs
+++ crates/api_crud/src/post/read.rs
@@ -1,16 +1,16 @@
 use crate::PerformCrud;
 use actix_web::web::Data;
 use tinyboards_api_common::{
+    data::TinyBoardsContext,
     post::{GetPost, GetPostResponse, PostIdPath},
-    utils::{blocking, get_user_view_from_jwt_opt, check_private_instance}, data::TinyBoardsContext,
+    utils::{blocking, check_private_instance, get_user_view_from_jwt_opt},
 };
 
 use tinyboards_utils::TinyBoardsError;
 
-use tinyboards_db_views::structs::{
-    PostView,
-    BoardView,
-    BoardModeratorView,
+use tinyboards_db_views::{
+    structs::{BoardModeratorView, BoardView, PostView},
+    DeleteableOrRemoveable,
 };
 
 #[async_trait::async_trait(?Send)]
@@ -24,24 +24,22 @@ impl<'des> PerformCrud<'des> for GetPost {
         path: Self::Route,
         auth: Option<&str>,
     ) -> Result<GetPostResponse, TinyBoardsError> {
-
         let _data = self;
-        
-        let user_view =
-             get_user_view_from_jwt_opt(auth, context.pool(), context.master_key()).await?;
-        
-        // check to see if instance is set to private before listing post
-        check_private_instance(
-            &user_view, 
-            context.pool()
-        )
-        .await?;
 
-        let user_id = user_view.map(|u| u.user.id);
+        let user_view =
+            get_user_view_from_jwt_opt(auth, context.pool(), context.master_key()).await?;
+
+        // check to see if instance is set to private before listing post
+        check_private_instance(&user_view, context.pool()).await?;
+
+        let user_id = match user_view {
+            Some(ref user_view) => Some(user_view.user.id),
+            None => None,
+        };
 
         let post_id = path.post_id;
 
-        let post_view = blocking(context.pool(), move |conn| {
+        let mut post_view = blocking(context.pool(), move |conn| {
             PostView::read(conn, post_id, user_id)
         })
         .await?
@@ -50,9 +48,11 @@ impl<'des> PerformCrud<'des> for GetPost {
             TinyBoardsError::err_500()
         })?;
 
+        post_view.hide_if_removed_or_deleted(user_view.as_ref());
+
         let _post_id = post_view.post.id;
 
-        // mark read here 
+        // mark read here
 
         let board_id = post_view.board.id;
         let board_view = blocking(context.pool(), move |conn| {
@@ -67,19 +67,17 @@ impl<'des> PerformCrud<'des> for GetPost {
         // blank out deleted or removed info here
 
         let moderators = blocking(context.pool(), move |conn| {
-            BoardModeratorView::for_board(conn, board_id)
-                .map_err(|e| {
-                    eprintln!("ERROR: {}", e);
-                    TinyBoardsError::err_500()
-                })
+            BoardModeratorView::for_board(conn, board_id).map_err(|e| {
+                eprintln!("ERROR: {}", e);
+                TinyBoardsError::err_500()
+            })
         })
         .await??;
 
-        Ok(GetPostResponse { 
-            post_view, 
-            board_view, 
-            moderators
-         })
-
+        Ok(GetPostResponse {
+            post_view,
+            board_view,
+            moderators,
+        })
     }
 }
