5bbbacc4c981930c0628e0789ce1898f4a04ad30
kroner <kronusdev@protonmail.com>
2022-11-22 20:44:30 -0600

    tweaks to integrate pictrs endpoints to the BE

--- crates/api_common/src/data.rs
+++ crates/api_common/src/data.rs
@@ -1,5 +1,5 @@
 use tinyboards_db::{models::secret::Secret, utils::DbPool};
-use tinyboards_utils::settings::{structs::Settings, SETTINGS};
+use tinyboards_utils::{settings::{structs::Settings, SETTINGS}, rate_limit::RateLimitCell};
 use reqwest_middleware::ClientWithMiddleware;
 
 
@@ -9,6 +9,7 @@ pub struct TinyBoardsContext {
     client: ClientWithMiddleware,
     settings: Settings,
     master_key: Secret,
+    rate_limit_cell: RateLimitCell,
 }
 
 impl TinyBoardsContext {
@@ -17,12 +18,14 @@ impl TinyBoardsContext {
         client: ClientWithMiddleware,
         settings: Settings,
         master_key: Secret,
+        settings_updated_channel: RateLimitCell,
     ) -> TinyBoardsContext {
         TinyBoardsContext {
             pool,
             client,
             settings,
             master_key,
+            rate_limit_cell: settings_updated_channel,
         }
     }
 
@@ -41,6 +44,10 @@ impl TinyBoardsContext {
     pub fn master_key(&self) -> &Secret {
         &self.master_key
     }
+
+    pub fn settings_updated_channel(&self) -> &RateLimitCell {
+        &&self.rate_limit_cell
+    }
 }
 
 impl Clone for TinyBoardsContext {
@@ -50,6 +57,7 @@ impl Clone for TinyBoardsContext {
             client: self.client.clone(),
             settings: self.settings.clone(),
             master_key: self.master_key.clone(),
+            rate_limit_cell: self.rate_limit_cell.clone(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ use tinyboards_api_common::{data::TinyBoardsContext, request::build_user_agent, 
 use tinyboards_db::models::secret::Secret;
 use tinyboards_db::utils::get_database_url_from_env;
 use tinyboards_server::{
-    api_routes, init_logging, root_span_builder::QuieterRootSpanBuilder, scheduled_tasks,
+    api_routes, init_logging, root_span_builder::QuieterRootSpanBuilder, scheduled_tasks, media
 };
 use tinyboards_utils::{
     error::TinyBoardsError,
-    rate_limit::{rate_limiter::RateLimiter, RateLimit},
+    rate_limit::{rate_limiter::RateLimiter, RateLimit, RateLimitCell},
     settings::SETTINGS,
 };
 use tracing_actix_web::TracingLogger;
@@ -80,6 +80,8 @@ async fn main() -> Result<(), TinyBoardsError> {
         rate_limit_config: settings.rate_limit.to_owned().unwrap_or_default(),
     };
 
+    let rate_limit_cell = RateLimitCell::new(settings.rate_limit.to_owned().unwrap_or_default()).await;
+
     // init the secret
     let conn = &mut pool.get().map_err(|_| {
         TinyBoardsError::from_message(
@@ -111,6 +113,10 @@ async fn main() -> Result<(), TinyBoardsError> {
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .build();
 
+    let pictrs_client = ClientBuilder::new(reqwest_client.clone())
+        .with(TracingMiddleware::default())
+        .build();
+
     let settings_bind = settings.clone();
     HttpServer::new(move || {
         let context = TinyBoardsContext::create(
@@ -118,14 +124,17 @@ async fn main() -> Result<(), TinyBoardsError> {
             client.clone(),
             settings.clone(),
             secret.clone(),
+            rate_limit_cell.clone(),
         );
         let rate_limiter = rate_limiter.clone();
         App::new()
             .wrap(actix_web::middleware::Logger::default())
             .wrap(TracingLogger::<QuieterRootSpanBuilder>::new())
             .app_data(Data::new(context))
-            .app_data(Data::new(rate_limiter.clone()))
+            .app_data(Data::new(rate_limit_cell.clone()))
             .configure(|cfg| api_routes::config(cfg, &rate_limiter))
+            // the extra routes
+            .configure(|cfg| media::config(cfg, pictrs_client.clone(), rate_limit_cell))
             .service(fs::Files::new("/assets", "./assets"))
     })
     .bind((settings_bind.bind, settings_bind.port))
