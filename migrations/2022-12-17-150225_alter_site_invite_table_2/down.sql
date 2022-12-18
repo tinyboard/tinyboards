f8c7955bfd94421cb4258e08c9c380b95f4be2a4
kroner <kronusdev@protonmail.com>
2022-12-18 14:46:45 -0600

    sql fix

--- migrations/2022-12-17-150225_alter_site_invite_table_2/down.sql
+++ migrations/2022-12-17-150225_alter_site_invite_table_2/down.sql
@@ -1,2 +1,1 @@
 alter table site_invite add column validated boolean default false not null;
-alter table site_invite rename column created to published;
--- migrations/2022-12-17-150225_alter_site_invite_table_2/up.sql
+++ migrations/2022-12-17-150225_alter_site_invite_table_2/up.sql
@@ -1,2 +1,1 @@
 alter table site_invite drop column validated;
-alter table site_invite rename column published to created;
