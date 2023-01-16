5141974ab399c626c34ac83eb234b4698a6b98c6
sic <sicxnull@protonmail.com>
2023-01-15 20:42:40 -0500

    add to hosts file

--- docker/docker.sh
+++ docker/docker.sh
@@ -402,6 +402,11 @@ export const baseURL = "http://$SERVER_NAME:$HTTP_PORT/api/v1";
 EOF
 fi
 
+#override hosts
+if [ "$ENVIRONMENT" = "dev" ]; then
+echo "127.0.0.1  $SERVER_NAME" >> /etc/hosts
+fi
+
 #make scripts executable
 
 if [ "$ENVIRONMENT" = "prod" ]; then
