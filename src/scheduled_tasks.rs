01aa9e81d221dbafbc5cf3e89b8ce4e8762b1d55
kroner <kronusdev@protonmail.com>
2023-02-11 14:13:27 -0600

    more refactors

--- src/scheduled_tasks.rs
+++ src/scheduled_tasks.rs
@@ -1,34 +1,38 @@
 // Scheduler, and trait for .seconds(), .minutes(), etc.
 use clokwerk::{Scheduler, TimeUnits};
 // Import week days and WeekDay
-use diesel::sql_query;
+use diesel::{sql_query, PgConnection, Connection, RunQueryDsl};
 use std::{thread, time::Duration};
-use tinyboards_db::utils::{DbPool, get_conn};
 use tinyboards_utils::error::TinyBoardsError;
 use tracing::info;
-use diesel_async::RunQueryDsl;
 
 /// Schedules various cleanup tasks for tinyboards in a background thread
-pub async fn setup(pool: DbPool) -> Result<(), TinyBoardsError> {
+pub fn setup(db_url: String) -> Result<(), TinyBoardsError> {
     let mut scheduler = Scheduler::new();
     let mut frequent_scheduler = Scheduler::new();
 
-    update_banned_when_expired(&pool).await;
-    update_user_aggregates_rep(&pool).await;
+    let mut conn1 = PgConnection::establish(&db_url)
+        .expect("could not establish connection");
+
+    update_banned_when_expired(&mut conn1);
+    update_user_aggregates_rep(&mut conn1);
 
     // On startup, reindex the tables non-concurrently
-    reindex_aggregates_tables(&pool, true);
+    reindex_aggregates_tables(&mut conn1, true);
     
     scheduler
     .every(TimeUnits::hour(1)).run(move || {
-        update_banned_when_expired(&pool);
-        reindex_aggregates_tables(&pool, true);
+        update_banned_when_expired(&mut conn1);
+        reindex_aggregates_tables(&mut conn1, true);
     });
-    
+
+    let mut conn2 = PgConnection::establish(&db_url)
+        .expect("could not establish connection");
+
     frequent_scheduler
     .every(TimeUnits::minutes(5))
     .run(move || {
-        update_user_aggregates_rep(&pool);
+        update_user_aggregates_rep(&mut conn2);
     });
 
     // Manually run the scheduler in an event loop
@@ -41,37 +45,33 @@ pub async fn setup(pool: DbPool) -> Result<(), TinyBoardsError> {
 
 /// Reindex the aggregates tables every one hour
 /// This is necessary because hot_rank is actually a mutable function
-async fn reindex_aggregates_tables(pool: &DbPool, concurrently: bool) {
+fn reindex_aggregates_tables(conn: &mut PgConnection, concurrently: bool) {
     
     for table_name in &["post_aggregates", "comment_aggregates", "board_aggregates"] {
-        reindex_table(pool, table_name, concurrently).await;
+        reindex_table(conn, table_name, concurrently);
     }
 }
 
-async fn reindex_table(pool: &DbPool, table_name: &str, concurrently: bool) {
-    let conn = &mut get_conn(pool).await?;
+fn reindex_table(conn: &mut PgConnection, table_name: &str, concurrently: bool) {
     let concurrently_str = if concurrently { "concurrently" } else { "" };
     info!("Reindexing table {} {} ...", concurrently_str, table_name);
     let query = format!("reindex table {} {}", concurrently_str, table_name);
-    sql_query(query).execute(conn).await.expect("reindex table");
+    sql_query(query).execute(conn).expect("reindex table");
     info!("Done.");
 }
 
 /// Set banned to false after ban expires
-async fn update_banned_when_expired(pool: &DbPool) {
-    let conn = &mut get_conn(pool).await?;
+fn update_banned_when_expired(conn: &mut PgConnection) {
     info!("Updating banned column if it expires ...");
     let update_ban_expires_stmt =
         "update users set is_banned = false where is_banned = true and unban_date < now()";
     sql_query(update_ban_expires_stmt)
         .execute(conn)
-        .await
         .expect("update banned when expires");
 }
 
 /// update the rep calculation on user_aggregates
-async fn update_user_aggregates_rep(pool: &DbPool) {
-    let conn = &mut get_conn(pool).await?;
+fn update_user_aggregates_rep(conn: &mut PgConnection) {
     info!("Updating rep values on user_aggregates ...");
     let update_user_aggregates_rep_stmt = 
     "update user_aggregates ua
@@ -101,6 +101,5 @@ async fn update_user_aggregates_rep(pool: &DbPool) {
      where ua.user_id = calc.user_id;";
      sql_query(update_user_aggregates_rep_stmt)
          .execute(conn)
-         .await
          .expect("update user aggregates rep values");
 }
