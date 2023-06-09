15e36264144280163a64695c220643dac7ff83e0
kroner <kronusdev@protonmail.com>
2023-06-09 08:42:13 -0500

    more apub crate util functions

--- crates/apub/src/lib.rs
+++ crates/apub/src/lib.rs
@@ -1,11 +1,11 @@
 use serde::Serialize;
-use tinyboards_federation::config::Data;
+use tinyboards_federation::config::{Data, UrlVerifier};
 use tinyboards_api_common::data::TinyBoardsContext;
-use tinyboards_utils::TinyBoardsError;
+use tinyboards_utils::{TinyBoardsError, settings::structs::Settings};
 use url::Url;
 use tinyboards_db::{
-    models::apub::{activity::*, local_site::LocalSite, instance::Instance},
-    traits::Crud,
+    models::apub::{activity::*, local_site::LocalSite, instance::Instance, federation_allowlist::FederationAllowList},
+    traits::Crud, utils::DbPool,
 };
 
 pub mod api;
@@ -15,17 +15,104 @@ pub mod objects;
 pub const FEDERATION_HTTP_FETCH_LIMIT: u32 = 50;
 
 #[derive(Clone)]
+pub struct VerifyUrlData(pub DbPool);
+
+#[async_trait::async_trait]
+impl UrlVerifier for VerifyUrlData {
+    async fn verify(&self, url: &Url) -> Result<(), &'static str> {
+
+        let local_site_data = fetch_local_site_data(&self.0)
+            .await
+            .expect("read local site data");
+
+        check_ap_id_valid(url, &local_site_data)?;
+
+        Ok(())
+
+    }
+}
+
+
+#[derive(Clone)]
 pub(crate) struct LocalSiteData {
-  local_site: Option<LocalSite>,
+  local_site: LocalSite,
   allowed_instances: Vec<Instance>,
   blocked_instances: Vec<Instance>,
 }
 
-// pub(crate) fn check_ap_id_valid_with_strictness(
-//     ap_id: &Url,
-//     is_strict: bool,
+pub(crate) async fn fetch_local_site_data(pool: &DbPool) -> Result<LocalSiteData, diesel::result::Error> {
+    let local_site = LocalSite::read_local(pool).await?;
+    let allowed_instances = Instance::allow_list(pool).await?;
+    let blocked_instances = Instance::block_list(pool).await?;
+    Ok( LocalSiteData { local_site, allowed_instances, blocked_instances })
+}
 
-// )
+/// Checks if ap id is allowed for sending or receiving
+/// 
+/// In particular it will check for:
+///     - federation being enabled
+///     - the correct scheme (either http or https)
+///     - URL being in the allow list (if active)
+///     - URL not being in the block list (if active)
+#[tracing::instrument(skip(local_site_data))]
+fn check_ap_id_valid(
+    ap_id: &Url,
+    local_site_data: &LocalSiteData,
+) -> Result<(), &'static str> {
+    let domain = ap_id.domain().expect("ap id has domain.").to_string();
+
+    if !local_site_data.local_site.federation_enabled {
+        return Err("federation disabled.");
+    }
+
+    if local_site_data
+        .blocked_instances
+        .iter()
+        .any(|i| domain.eq(&i.domain)) {
+
+        return Err("domain is blocked.");
+    }
+
+    // only check this if instances are in the allow list
+    if !local_site_data.allowed_instances.is_empty() &&
+       !local_site_data
+        .allowed_instances
+        .iter()
+        .any(|i| domain.eq(&i.domain)) {
+            return Err("domain is not in allow list.");
+    }
+
+    Ok(())
+}
+
+pub(crate) fn check_ap_id_valid_with_strictness(
+    ap_id: &Url,
+    is_strict: bool,
+    local_site_data: &LocalSiteData,
+    settings: &Settings,
+) -> Result<(), TinyBoardsError> {
+
+    // only check the allow list if the ap_id is a board and allow list has some items in it
+    if is_strict && !local_site_data.allowed_instances.is_empty() {
+        // need to allow this explicitly because the apub receive might contain objects from the local instance
+        let mut allowed_and_local = local_site_data
+            .allowed_instances
+            .iter()
+            .map(|i| i.domain.clone())
+            .collect::<Vec<String>>();
+        let local_instance 
+            = settings.get_hostname_without_port()?;
+        allowed_and_local
+            .push(local_instance);
+        let domain 
+            = ap_id.domain().expect("apub id has domain").to_string();
+        
+        if !allowed_and_local.contains(&domain) {
+            return Err(TinyBoardsError::from_message(403, "federation forbidden by a strict allow list."));
+        }      
+    }
+    Ok(())
+}
 
 /// Store an activity in the database (sent or received).
 ///
--- crates/db/src/impls/apub/instance.rs
+++ crates/db/src/impls/apub/instance.rs
@@ -1,4 +1,4 @@
-use crate::schema::instance::dsl::*;
+use crate::schema::{instance, federation_allowlist, federation_blocklist};
 use crate::utils::{get_conn, DbPool};
 use crate::{
     models::apub::instance::*,
@@ -14,13 +14,13 @@ impl Crud for Instance {
 
     async fn read(pool: &DbPool, id_: i32) -> Result<Self, Error> {
         let conn = &mut get_conn(pool).await?;
-        instance.find(id_).first::<Self>(conn)
+        instance::table.find(id_).first::<Self>(conn)
         .await
     }
 
     async fn create(pool: &DbPool, form: &InstanceForm) -> Result<Self, Error> {
         let conn = &mut get_conn(pool).await?;
-        insert_into(instance)
+        insert_into(instance::table)
             .values(form)
             .get_result::<Self>(conn)
             .await
@@ -32,7 +32,7 @@ impl Crud for Instance {
         form: &InstanceForm,
     ) -> Result<Self, Error> {
         let conn = &mut get_conn(pool).await?;
-        diesel::update(instance.find(id_))
+        diesel::update(instance::table.find(id_))
             .set(form)
             .get_result::<Self>(conn)
             .await
@@ -40,7 +40,29 @@ impl Crud for Instance {
 
     async fn delete(pool: &DbPool, id_: i32) -> Result<usize, Error> {
         let conn = &mut get_conn(pool).await?;
-        diesel::delete(instance.find(id_)).execute(conn)
+        diesel::delete(instance::table.find(id_)).execute(conn)
         .await
     }
+}
+
+impl Instance {
+
+    pub async fn allow_list(pool: &DbPool) -> Result<Vec<Self>, Error> {
+        let conn = &mut get_conn(pool).await?;
+        instance::table
+            .inner_join(federation_allowlist::table)
+            .select(instance::all_columns)
+            .get_results(conn)
+            .await
+    }
+
+    pub async fn block_list(pool: &DbPool) -> Result<Vec<Self>, Error> {
+        let conn = &mut get_conn(pool).await?;
+        instance::table
+            .inner_join(federation_blocklist::table)
+            .select(instance::all_columns)
+            .get_results(conn)
+            .await        
+    }
+
 }
