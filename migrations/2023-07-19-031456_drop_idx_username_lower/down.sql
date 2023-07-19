d5bbb5db28a559f9d386c07c673cae7384e4b3f8
kroner <kronusdev@protonmail.com>
2023-07-18 22:28:18 -0500

    asdf

--- migrations/2023-07-19-031456_drop_idx_username_lower/down.sql
+++ migrations/2023-07-19-031456_drop_idx_username_lower/down.sql
@@ -1,1 +1,2 @@
+drop index idx_username_lower_and_instance_id;
 create unique index idx_user_name_lower on person (lower(name));
--- migrations/2023-07-19-031456_drop_idx_username_lower/up.sql
+++ migrations/2023-07-19-031456_drop_idx_username_lower/up.sql
@@ -1,1 +1,2 @@
 drop index idx_user_name_lower;
+create unique index idx_username_lower_and_instance_id on person (lower(name), instance_id);
